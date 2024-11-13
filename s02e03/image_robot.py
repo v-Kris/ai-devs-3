fd1b0539569d69bbae42c7aa64412b52bf12da7c
Krzysztof Obara <45977636+v-Kris@users.noreply.github.com>
2024-11-13 18:13:25 +0100

    Update image_robot.py

--- s02e03/image_robot.py
+++ s02e03/image_robot.py
@@ -39,10 +39,7 @@ try:
     
     # Get the image URL from the response
     image_url = response.data[0].url
-    # image_url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-TaVhM6pvPKbfl5EbylocW77I/user-0NnhYuR1inka90I7DroBShdd/img-zNmzfiBsK6kG9fddI1a2PCmT.png?st=2024-11-13T13%3A01%3A48Z&se=2024-11-13T15%3A01%3A48Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-11-12T19%3A51%3A15Z&ske=2024-11-13T19%3A51%3A15Z&sks=b&skv=2024-08-04&sig=BYJnowYfTXkKWyMtMuCffPLgOE/Fyev25CeqOFHqEIU%3D"
 
-    # image_url = "https://oaidalleapiprodscus.blob.core.windows.net/private/org-TaVhM6pvPKbfl5EbylocW77I/user-0NnhYuR1inka90I7DroBShdd/img-CBc3cutshFIPNJTtQrgdmj1S.png?st=2024-11-13T13%3A17%3A56Z&se=2024-11-13T15%3A17%3A56Z&sp=r&sv=2024-08-04&sr=b&rscd=inline&rsct=image/png&skoid=d505667d-d6c1-4a0a-bac7-5c84a87759f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-11-12T20%3A07%3A29Z&ske=2024-11-13T20%3A07%3A29Z&sks=b&skv=2024-08-04&sig=LYyHZc9fGjUcV6vKIMXjqBXVjAnbnkLpU5jM2PK80GQ%3D"
-    
     print(f"Generated image URL: {image_url}")
     
     # Prepare and send the report
